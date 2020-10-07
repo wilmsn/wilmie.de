f9040342c6a535079ba48594f59b19a2059f21a7
Norbert <mail@wilmie.de>
2020-10-07 16:03:24 +0200

    Umstellung auf configurierbare Sensoren mittels /etc/webserver/*_config.php

--- content/ladegrafik.php
+++ content/ladegrafik.php
@@ -10,35 +10,53 @@ $mobile_browser = is_mobile_browser();
   </head>
 <?php
 if($mobile_browser) { 
-?>
-<h1>Batteriespannung Wohnzimmer</h1>
-<img src="/content/diagramm.php?sensor1=133&range=6m&sensor1legend=Batterie&ymin=3.5&ymax=4.2&sizex=400">
-<h1>Batteriespannung Küche</h1>
-<img src="/content/diagramm.php?sensor1=12&range=6m&sensor1legend=Batterie&ymin=3.5&ymax=4.2&sizex=400">
-<h1>Batteriespannung Bastelzimmer</h1>
-<img src="/content/diagramm.php?sensor1=10&range=6m&sensor1legend=Batterie&ymin=3.5&ymax=4.2&sizex=400">
-<h1>Batteriespannung Schlafzimmer</h1>
-<img src="/content/diagramm.php?sensor1=161&range=6m&sensor1legend=Batterie&ymin=3.5&ymax=4.2&sizex=400">
-<h1>Batteriespannung Ankleidezimmer</h1>
-<img src="/content/diagramm.php?sensor1=181&range=6m&sensor1legend=Batterie&ymin=3.5&ymax=4.2&sizex=400">
-<h1>Batteriespannung Gästezimmer</h1>
-<img src="/content/diagramm.php?sensor1=201&range=6m&sensor1legend=Batterie&ymin=3.5&ymax=4.2&sizex=400">
-<?php
-} else { 
-?>
-<h1>Batteriespannung Wohnzimmer</h1>
-<img src="/content/diagramm.php?sensor1=133&range=6m&sensor1legend=Batterie&ymin=3.5&ymax=4.2&sizex=1200">
-<h1>Batteriespannung Küche</h1>
-<img src="/content/diagramm.php?sensor1=12&range=6m&sensor1legend=Batterie&ymin=3.5&ymax=4.2&sizex=1200">
-<h1>Batteriespannung Bastelzimmer</h1>
-<img src="/content/diagramm.php?sensor1=10&range=6m&sensor1legend=Batterie&ymin=3.5&ymax=4.2&sizex=1200">
-<h1>Batteriespannung Schlafzimmer</h1>
-<img src="/content/diagramm.php?sensor1=161&range=6m&sensor1legend=Batterie&ymin=3.5&ymax=4.2&sizex=1200">
-<h1>Batteriespannung Ankleidezimmer</h1>
-<img src="/content/diagramm.php?sensor1=181&range=6m&sensor1legend=Batterie&ymin=3.5&ymax=4.2&sizex=1200">
-<h1>Batteriespannung Gästezimmer</h1>
-<img src="/content/diagramm.php?sensor1=201&range=6m&sensor1legend=Batterie&ymin=3.5&ymax=4.2&sizex=1200">
+    $sizex = 400;
+} else {
+    $sizex = 1200;
+}
+if(isset($batt1_name)) {
+  print "<h1>Batteriespannung ".$batt1_name."</h1>";
+  print "<img src='/content/diagramm.php?sensor1=".$batt1_sensor."&range=6m&sensor1legend=Batterie&ymin=".$batt1_umin."&ymax=".$batt1_umax."&sizex=".$sizex."'>";
+}
+if(isset($batt2_name)) {
+  print "<h1>Batteriespannung ".$batt2_name."</h1>";
+  print "<img src='/content/diagramm.php?sensor1=".$batt2_sensor."&range=6m&sensor1legend=Batterie&ymin=".$batt2_umin."&ymax=".$batt2_umax."&sizex=".$sizex."'>";
+}
+if(isset($batt3_name)) {
+  print "<h1>Batteriespannung ".$batt3_name."</h1>";
+  print "<img src='/content/diagramm.php?sensor1=".$batt3_sensor."&range=6m&sensor1legend=Batterie&ymin=".$batt3_umin."&ymax=".$batt3_umax."&sizex=".$sizex."'>";
+}
+if(isset($batt4_name)) {
+  print "<h1>Batteriespannung ".$batt4_name."</h1>";
+  print "<img src='/content/diagramm.php?sensor1=".$batt4_sensor."&range=6m&sensor1legend=Batterie&ymin=".$batt4_umin."&ymax=".$batt4_umax."&sizex=".$sizex."'>";
+}
+if(isset($batt5_name)) {
+  print "<h1>Batteriespannung ".$batt5_name."</h1>";
+  print "<img src='/content/diagramm.php?sensor1=".$batt5_sensor."&range=6m&sensor1legend=Batterie&ymin=".$batt5_umin."&ymax=".$batt5_umax."&sizex=".$sizex."'>";
+}
+if(isset($batt6_name)) {
+  print "<h1>Batteriespannung ".$batt6_name."</h1>";
+  print "<img src='/content/diagramm.php?sensor1=".$batt6_sensor."&range=6m&sensor1legend=Batterie&ymin=".$batt6_umin."&ymax=".$batt6_umax."&sizex=".$sizex."'>";
+}
+if(isset($batt7_name)) {
+  print "<h1>Batteriespannung ".$batt7_name."</h1>";
+  print "<img src='/content/diagramm.php?sensor1=".$batt7_sensor."&range=6m&sensor1legend=Batterie&ymin=".$batt7_umin."&ymax=".$batt7_umax."&sizex=".$sizex."'>";
+}
+if(isset($batt8_name)) {
+  print "<h1>Batteriespannung ".$batt8_name."</h1>";
+  print "<img src='/content/diagramm.php?sensor1=".$batt8_sensor."&range=6m&sensor1legend=Batterie&ymin=".$batt8_umin."&ymax=".$batt8_umax."&sizex=".$sizex."'>";
+}
+if(isset($batt9_name)) {
+  print "<h1>Batteriespannung ".$batt9_name."</h1>";
+  print "<img src='/content/diagramm.php?sensor1=".$batt9_sensor."&range=6m&sensor1legend=Batterie&ymin=".$batt9_umin."&ymax=".$batt9_umax."&sizex=".$sizex."'>";
+}
+if(isset($batt10_name)) {
+  print "<h1>Batteriespannung ".$batt10_name."</h1>";
+  print "<img src='/content/diagramm.php?sensor1=".$batt10_sensor."&range=6m&sensor1legend=Batterie&ymin=".$batt10_umin."&ymax=".$batt10_umax."&sizex=".$sizex."'>";
+}
+if(isset($batt11_name)) {
+  print "<h1>Batteriespannung ".$batt11_name."</h1>";
+  print "<img src='/content/diagramm.php?sensor1=".$batt11_sensor."&range=6m&sensor1legend=Batterie&ymin=".$batt11_umin."&ymax=".$batt11_umax."&sizex=".$sizex."'>";
+}
 
-<?php
-} 
 ?>
