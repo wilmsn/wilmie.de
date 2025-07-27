7c35314cc8b696fcb32d43eb92ca44c157c59d97
Norbert <mail@wilmie.de>
2025-07-27 16:05:03 +0200

    Haussteuerungüberarbeitet

--- content/wetter_content.php
+++ content/wetter_content.php
@@ -1,5 +1,5 @@
 <?php
-$instance="intern";
+$instance="test";
 require_once ('/etc/webserver/'.$instance.'_config.php');
 require_once ($webroot.'/php_inc/check_mobile.php');
 //$db_dh = new mysqli($db_dh_server, $db_dh_user, $db_dh_pass, $db_dh_db);
